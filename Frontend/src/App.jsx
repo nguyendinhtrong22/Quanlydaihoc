29a4c9e1879119ffdf6c384029211659fe41f3b9
nguyendinhtrong <nguyendinhtrongctm@gamil>
2026-08-11 10:54:02 +0700

    Nguyễn Đình Trọng

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { StrictMode } from "react";
+function App() {
+  return (
+    <StrictMode>
+      <div className="container">
+        <div className="header">WebGIS - Quản lý trường đại học</div>
+
+        <div className="body"></div>
+      </div>
+    </StrictMode>
+  );
+}
+export default App;
